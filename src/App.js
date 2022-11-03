a5d4cf3f4da1d79da3fbc6f76d9c65720973c812
Maria Gabriela Galindo <gabi_who@live.cm>
2022-11-03 17:01:13 -0300

    Adiciona requisito 1

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
+import Login from './pages/Login';
+import Search from './pages/Search';
+import Album from './pages/Album';
+import Favorites from './pages/Favorites';
+import NotFound from './pages/NotFound';
+import Profile from './pages/Profile';
+import ProfileEdit from './pages/ProfileEdit';
 
 class App extends React.Component {
   render() {
-    return (<p>Olá TrybeTunes</p>);
+    return (
+      <div>
+        <p>TrybeTunes</p>
+        <Route path="/" component={ Login } />
+        <Route path="/search" component={ Search } />
+        <Route path="/album/:id" component={ Album } />
+        <Route path="/favorites" component={ Favorites } />
+        <Route path="/profile" component={ Profile } />
+        <Route path="/profile/edit" component={ ProfileEdit } />
+        <Route path="*" component={ NotFound } />
+      </div>
+    );
   }
 }
 
